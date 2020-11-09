2b3c1d8c1cf3c61430ad8bf9a39cee9ed8c66277
chenyh <1449101002@qq.com>
2020-11-09 21:25:48 +0800

    weather界面的修改
    以及其他的小修改

--- pages/tools/weather/weather.js
+++ pages/tools/weather/weather.js
@@ -1,115 +1,116 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-    heweatherKey: "711e2e6862d84ce78811845afb652338",  
+    heweatherKey: "711e2e6862d84ce78811845afb652338",
     cityMessage: {},
     now: {},
     today: {},
-    tomorrow: {}
+    tomorrow: {},
+    dayAfterTomorrow: {},
   },
 
   /**
    * picker组件绑定事件 bindchange="picker_getWeather"
-   * @param {*} location 
+   * @param {*} location
    */
   picker_getWeather: function (location) {
     var that = this;
     var _location = location.detail.value[2];
     var _adm = location.detail.value[1];
     wx.request({
-      url: 'https://geoapi.heweather.net/v2/city/lookup',
+      url: "https://geoapi.heweather.net/v2/city/lookup",
       data: {
         location: _location,
         adm: _adm,
-        key: that.data.heweatherKey
+        key: that.data.heweatherKey,
       },
       success: (result) => {
         that.setData({
-          cityMessage: result.data.location[0]
-        })
+          cityMessage: result.data.location[0],
+        });
         this.getWeather("now");
         this.getWeather("3d");
       },
-      complete: () => {}
-    });  
+      complete: () => {},
+    });
   },
 
   getWeather: function (timing) {
     var that = this;
     wx.request({
-      url: 'https://devapi.heweather.net/v7/weather/' + timing,
+      url: "https://devapi.heweather.net/v7/weather/" + timing,
       data: {
         location: that.data.cityMessage.id,
-        key: that.data.heweatherKey
+        key: that.data.heweatherKey,
       },
       success: (result) => {
         if (timing == "3d") {
           that.setData({
             today: result.data.daily[0],
-            tomorrow: result.data.daily[1]
+            tomorrow: result.data.daily[1],
+            dayAfterTomorrow: result.data.daily[2],
           });
-          console.log(that.data.today);
           console.log(that.data.tomorrow);
+          console.log(that.data.dayAfterTomorrow);
         } else if (timing == "now") {
           that.setData({
-            now: result.data.now
-          })
+            now: result.data.now,
+          });
           console.log(that.data.now);
         }
-      }
-    }); 
+      },
+    });
   },
 
   updateWeatherData: function (_location, _adm) {
     var that = this;
     wx.request({
-      url: 'https://geoapi.heweather.net/v2/city/lookup',
+      url: "https://geoapi.heweather.net/v2/city/lookup",
       data: {
         location: _location,
         adm: _adm,
-        key: that.data.heweatherKey
+        key: that.data.heweatherKey,
       },
       success: (result) => {
         that.setData({
-          cityMessage: result.data.location[0]
-        })
+          cityMessage: result.data.location[0],
+        });
         // 更新now
         wx.request({
-          url: 'https://devapi.heweather.net/v7/weather/now',
+          url: "https://devapi.heweather.net/v7/weather/now",
           data: {
             location: that.data.cityMessage.id,
-            key: that.data.heweatherKey
+            key: that.data.heweatherKey,
           },
           success: (_result) => {
             that.setData({
-              now: _result.data.now
+              now: _result.data.now,
             });
             wx.hideNavigationBarLoading();
             wx.stopPullDownRefresh();
-          }
+          },
         });
         // 更新today&tomorrow
         wx.request({
-          url: 'https://devapi.heweather.net/v7/weather/3d',
+          url: "https://devapi.heweather.net/v7/weather/3d",
           data: {
             location: that.data.cityMessage.id,
-            key: that.data.heweatherKey
+            key: that.data.heweatherKey,
           },
           success: (_result) => {
             that.setData({
               today: _result.data.daily[0],
-              tomorrow: _result.data.daily[1]
+              tomorrow: _result.data.daily[1],
+              dayAfterTomorrow: _result.data.daily[2],
             });
-          }
+          },
         });
-      }
+      },
     });
-
   },
-
 
   /**
    * 生命周期函数--监听页面加载
@@ -119,10 +120,13 @@ Page({
     this.setData({
       cityMessage: {
         adm2: "郑州",
-        name: "金水"
-      }
+        name: "金水",
+      },
     });
-    this.updateWeatherData(this.data.cityMessage.name, this.data.cityMessage.adm2);
+    this.updateWeatherData(
+      this.data.cityMessage.name,
+      this.data.cityMessage.adm2
+    );
   },
 
   /**
@@ -130,6 +134,9 @@ Page({
    */
   onPullDownRefresh: function () {
     wx.showNavigationBarLoading();
-    this.updateWeatherData(this.data.cityMessage.name, this.data.cityMessage.adm2);
+    this.updateWeatherData(
+      this.data.cityMessage.name,
+      this.data.cityMessage.adm2
+    );
   },
-})
+});
